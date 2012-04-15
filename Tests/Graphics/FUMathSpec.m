4d8e9324b8c4bf8e08ab42713c74341460cdbd02
David Hart <david@hart-dev.com>
2012-04-15 12:38:19 +0200

    Added some math testing specs

---
+++ Tests/Graphics/FUMathSpec.m
@@ -0,0 +1,27 @@
+//
+//  FUSceneSpec.m
+//  Fuji
+//
+//  Created by Hart David on 24.02.12.
+//  Copyright (c) 2012 hart[dev]. All rights reserved.
+//
+
+#include "Prefix.pch"
+#import "Fuji.h"
+
+
+SPEC_BEGIN(FUMathSpec)
+
+describe(@"FUMath", ^{
+	it(@"has a GLKVector2 constant that equals (0, 0)", ^{
+		GLKVector2 zero = GLKVector2Make(0, 0);
+		expect(GLKVector2AllEqualToVector2(GLKVector2Zero, zero)).to.beTruthy();
+	});
+		
+	it(@"has a GLKVector2 constant that equals (1, 1)", ^{
+		GLKVector2 one = GLKVector2Make(1, 1);
+		expect(GLKVector2AllEqualToVector2(GLKVector2One, one)).to.beTruthy();
+	});
+});
+
+SPEC_END
