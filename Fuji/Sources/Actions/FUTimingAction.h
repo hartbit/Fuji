b15ff6d6be8fc9218faa301c59ceefff940afb07
David Hart <david@hart-dev.com>
2012-05-11 15:41:08 +0200

    Created the definitions for the timing functions

--- Fuji/Sources/Actions/FUTimingAction.h
+++ Fuji/Sources/Actions/FUTimingAction.h
@@ -16,6 +16,39 @@
 typedef FUTime (^FUTimingFunction)(FUTime t);
 
 
+OBJC_EXPORT const FUTimingFunction FUTimingLinear;
+OBJC_EXPORT const FUTimingFunction FUTimingQuadIn;
+OBJC_EXPORT const FUTimingFunction FUTimingQuadOut;
+OBJC_EXPORT const FUTimingFunction FUTimingQuadInOut;
+OBJC_EXPORT const FUTimingFunction FUTimingCubicIn;
+OBJC_EXPORT const FUTimingFunction FUTimingCubicOut;
+OBJC_EXPORT const FUTimingFunction FUTimingCubicInOut;
+OBJC_EXPORT const FUTimingFunction FUTimingQuartIn;
+OBJC_EXPORT const FUTimingFunction FUTimingQuartOut;
+OBJC_EXPORT const FUTimingFunction FUTimingQuartInOut;
+OBJC_EXPORT const FUTimingFunction FUTimingQuintIn;
+OBJC_EXPORT const FUTimingFunction FUTimingQuintOut;
+OBJC_EXPORT const FUTimingFunction FUTimingQuintInOut;
+OBJC_EXPORT const FUTimingFunction FUTimingSinIn;
+OBJC_EXPORT const FUTimingFunction FUTimingSinOut;
+OBJC_EXPORT const FUTimingFunction FUTimingSinInOut;
+OBJC_EXPORT const FUTimingFunction FUTimingExpoIn;
+OBJC_EXPORT const FUTimingFunction FUTimingExpoOut;
+OBJC_EXPORT const FUTimingFunction FUTimingExpoInOut;
+OBJC_EXPORT const FUTimingFunction FUTimingCircIn;
+OBJC_EXPORT const FUTimingFunction FUTimingCircOut;
+OBJC_EXPORT const FUTimingFunction FUTimingCircInOut;
+OBJC_EXPORT const FUTimingFunction FUTimingBackIn;
+OBJC_EXPORT const FUTimingFunction FUTimingBackOut;
+OBJC_EXPORT const FUTimingFunction FUTimingBackInOut;
+OBJC_EXPORT const FUTimingFunction FUTimingElasticIn;
+OBJC_EXPORT const FUTimingFunction FUTimingElasticOut;
+OBJC_EXPORT const FUTimingFunction FUTimingElasticInOut;
+OBJC_EXPORT const FUTimingFunction FUTimingBounceIn;
+OBJC_EXPORT const FUTimingFunction FUTimingBounceOut;
+OBJC_EXPORT const FUTimingFunction FUTimingBounceInOut;
+
+
 @interface FUTimingAction : FUFiniteAction
 
 - (id)initWithAction:(id<FUAction>)action function:(FUTimingFunction)function;
--- Fuji/Sources/Actions/FUTimingAction.m
+++ Fuji/Sources/Actions/FUTimingAction.m
@@ -17,6 +17,132 @@ static NSString* const FUActionNilMessage = @"Expected 'action' to not be nil";
 static NSString* const FUFunctionNullMessage = @"Expected 'function' to not be NULL";
 
 
+const FUTimingFunction FUTimingLinear = ^(FUTime t) {
+	return t;
+};
+
+const FUTimingFunction FUTimingQuadIn = ^(FUTime t) {
+	return t;
+};
+
+const FUTimingFunction FUTimingQuadOut = ^(FUTime t) {
+	return t;
+};
+
+const FUTimingFunction FUTimingQuadInOut = ^(FUTime t) {
+	return t;
+};
+
+const FUTimingFunction FUTimingCubicIn = ^(FUTime t) {
+	return t;
+};
+
+const FUTimingFunction FUTimingCubicOut = ^(FUTime t) {
+	return t;
+};
+
+const FUTimingFunction FUTimingCubicInOut = ^(FUTime t) {
+	return t;
+};
+
+const FUTimingFunction FUTimingQuartIn = ^(FUTime t) {
+	return t;
+};
+
+const FUTimingFunction FUTimingQuartOut = ^(FUTime t) {
+	return t;
+};
+
+const FUTimingFunction FUTimingQuartInOut = ^(FUTime t) {
+	return t;
+};
+
+const FUTimingFunction FUTimingQuintIn = ^(FUTime t) {
+	return t;
+};
+
+const FUTimingFunction FUTimingQuintOut = ^(FUTime t) {
+	return t;
+};
+
+const FUTimingFunction FUTimingQuintInOut = ^(FUTime t) {
+	return t;
+};
+
+const FUTimingFunction FUTimingSinIn = ^(FUTime t) {
+	return t;
+};
+
+const FUTimingFunction FUTimingSinOut = ^(FUTime t) {
+	return t;
+};
+
+const FUTimingFunction FUTimingSinInOut = ^(FUTime t) {
+	return t;
+};
+
+const FUTimingFunction FUTimingExpoIn = ^(FUTime t) {
+	return t;
+};
+
+const FUTimingFunction FUTimingExpoOut = ^(FUTime t) {
+	return t;
+};
+
+const FUTimingFunction FUTimingExpoInOut = ^(FUTime t) {
+	return t;
+};
+
+const FUTimingFunction FUTimingCircIn = ^(FUTime t) {
+	return t;
+};
+
+const FUTimingFunction FUTimingCircOut = ^(FUTime t) {
+	return t;
+};
+
+const FUTimingFunction FUTimingCircInOut = ^(FUTime t) {
+	return t;
+};
+
+const FUTimingFunction FUTimingBackIn = ^(FUTime t) {
+	return t;
+};
+
+const FUTimingFunction FUTimingBackOut = ^(FUTime t) {
+	return t;
+};
+
+const FUTimingFunction FUTimingBackInOut = ^(FUTime t) {
+	return t;
+};
+
+const FUTimingFunction FUTimingElasticIn = ^(FUTime t) {
+	return t;
+};
+
+const FUTimingFunction FUTimingElasticOut = ^(FUTime t) {
+	return t;
+};
+
+const FUTimingFunction FUTimingElasticInOut = ^(FUTime t) {
+	return t;
+};
+
+const FUTimingFunction FUTimingBounceIn = ^(FUTime t) {
+	return t;
+};
+
+const FUTimingFunction FUTimingBounceOut = ^(FUTime t) {
+	return t;
+};
+
+const FUTimingFunction FUTimingBounceInOut = ^(FUTime t) {
+	return t;
+};
+
+
+
 @implementation FUTimingAction
 
 #pragma mark - Initialization
